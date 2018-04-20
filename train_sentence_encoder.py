bb1dfcc64ce81144ba125a3ff8d4f803cde204bd
diego <diego.andersen@protonmail.com>
2018-04-20 13:29:33 +0100

    Split off model training flags into config.py so you don't have to commit every time you change an option. Config saved as template file, local version will want a 'config.py'

--- train_sentence_encoder.py
+++ train_sentence_encoder.py
@@ -11,45 +11,7 @@ import tensorflow as tf
 
 import neural_summarizer
 from blackbox_data_reader import load_data, DOMReader
-
-
-flags = tf.flags
-
-# data
-flags.DEFINE_string('data_dir',    '/home/ubuntu/data_store/training_data/10',   'data directory. Should contain train.txt/valid.txt/test.txt with input data')
-flags.DEFINE_string('train_dir',   'cv',     'training directory (models and summaries are saved there periodically)')
-flags.DEFINE_string('load_model',   None,    '(optional) filename of the model to load. Useful for re-starting training from a checkpoint')
-flags.DEFINE_string('master_file', 'main/main_10.csv', 'path to master .csv containing locations of all training data')
-
-# model params
-flags.DEFINE_string ('model_choice',    'lstm',                         'model choice')
-flags.DEFINE_string ('embedding_path',  None,                           'pretrained emebdding path')
-flags.DEFINE_integer('rnn_size',        650,                            'size of LSTM internal state')
-flags.DEFINE_integer('highway_layers',  2,                              'number of highway layers')
-flags.DEFINE_integer('word_embed_size', 3,                             'dimensionality of word embeddings')
-flags.DEFINE_string ('kernels',         '[1,2,3,4,5,6,7]',              'CNN kernel widths')
-flags.DEFINE_string ('kernel_features', '[50,100,150,200,200,200,200]', 'number of features in the CNN kernel')
-flags.DEFINE_integer('rnn_layers',      2,                              'number of layers in the LSTM')
-flags.DEFINE_float  ('dropout',         0.5,                            'dropout. 0 = no dropout')
-
-# optimization
-flags.DEFINE_float  ('learning_rate_decay', 0.5,  'learning rate decay')
-flags.DEFINE_float  ('learning_rate',       1.0,  'starting learning rate')
-flags.DEFINE_float  ('decay_when',          1.0,  'decay if validation perplexity does not improve by more than this much')
-flags.DEFINE_float  ('param_init',          0.05, 'initialize parameters at')
-flags.DEFINE_integer('batch_size',          20,   'number of sequences to train on in parallel')
-flags.DEFINE_integer('max_epochs',          25,   'number of full passes through the training data')
-flags.DEFINE_float  ('max_grad_norm',       5.0,  'normalize gradients at')
-flags.DEFINE_integer('max_doc_length',    3000,   'max_doc_length')
-flags.DEFINE_integer('max_line_length',     130,   'maximum sentence length')
-
-# bookkeeping
-flags.DEFINE_integer('seed',           3435, 'random number generator seed')
-flags.DEFINE_integer('print_every',    5,    'how often to print current loss')
-flags.DEFINE_string ('EOS',            '+',  '<EOS> symbol. should be a single unused character (like +) for PTB and blank for others')
-
-FLAGS = flags.FLAGS
-
+from config import FLAGS
 
 def run_test(session, m, data, batch_size, num_steps):
     """Runs the model on the given data."""
